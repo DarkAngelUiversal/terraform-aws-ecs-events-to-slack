8c3230b6d5a9de19e6730a93da4f8e9177dc4810
aleksandr kuznetsov <sasha.ru.010@gmail.com>
2025-04-21 23:02:12 +0300

    delete kms_master_key_id for aws_sns_topic ready

--- sns.tf
+++ sns.tf
@@ -1,10 +1,12 @@
 resource "aws_sns_topic" "this" {
   #tfsec:ignore:aws-sns-topic-encryption-use-cmk
+  #checkov:skip=CKV_AWS_26:Ensure all data stored in the SNS topic is encrypted
+  #checkov:skip=CKV_AWS_27:Ensure all data stored in the SNS topic is encrypted
   count = var.sns_topic_arn == "" ? 1 : 0
   name  = var.aws_sns_topic_name
   tags  = var.tags
 
-  kms_master_key_id = "alias/aws/sns"
+  # kms_master_key_id = "alias/aws/sns"
 }
 
 resource "aws_sns_topic_policy" "this" {
